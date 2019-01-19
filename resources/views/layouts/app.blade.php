1abf13574529b0175c04d0e2523af6bfe6c0f737
ding <707170913@qq.com>
2019-01-19 11:45:59 +0800

    提交jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}" defer></script>
     {{--<script src="{{ asset('js/bootstrap.min.js') }}" ></script>--}}
 
     <!-- Fonts -->
